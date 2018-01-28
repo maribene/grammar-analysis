948899fe83bfb6b05f04ae4cd97d420a6c10caa5
Maria Benet <mabegar22@gmail.com>
2018-01-28 18:38:41 +0100

    Makes a better grammar and takes the sentences from user input

--- grammar_analysis.py
+++ grammar_analysis.py
@@ -4,68 +4,36 @@ from nltk.probability import FreqDist
 
 
 grammar = nltk.CFG.fromstring("""
-  S -> NP VP
-  VP -> V NP | V NP PP | V NP NP | V
-  PP -> P NP
-  V -> "gives" | "sleeps" | "eats" | "finds"
-  NP -> "Per" | "Kari" | "Ola" | "dinner" | "book" | Det N
-  Det -> "a" | "the"
-  N -> "book"
-  P -> "to"
+ S -> NP VP
+ VP -> Vt NP | Vt NP PP | Vt NP NP | Vi
+ PP -> P NP
+ Vi -> "sleeps"| "eats"
+ Vt -> "gives" | "eats" | "finds"
+ NP -> "Per" | "Kari" | "Ola" | "dinner" | "book" | Det N
+ Det -> "a" | "the"
+ N -> "book"
+ P -> "to"
   """)
 
-sentence = "Per gives a book to Kari".split()
+input_sentence = raw_input("Write a sentence: ")
+
+sentence = input_sentence.split()
 rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
+trees = rd_parser.parse(sentence)
+
+for tree in trees:
     print(tree)
 
-sentence = "Kari gives Per the book".split()
-rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
-    print(tree)
+"""
+"Per gives a book to Kari"
+"Kari gives Per the book"
+"Ola sleeps"
+"Kari eats"
+"Kari eats dinner"
+"Per finds the book"
 
-sentence = "Ola sleeps".split()
-rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
-    print(tree)
+---not grammatically correct
 
-sentence = "Kari eats".split()
-rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
-    print(tree)
-
-sentence = "Kari eats dinner".split()
-rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
-    print(tree)
-
-sentence = "Per finds the book".split()
-rd_parser = nltk.RecursiveDescentParser(grammar)
-for tree in rd_parser.parse(sentence):
-    print(tree)
-
-
-grammar2 = nltk.CFG.fromstring("""
-  S -> NP VP
-  VP -> Vt NP | Vt NP PP | Vt NP NP | Vi
-  PP -> P NP
-  Vi -> "sleeps"| "eats"
-  Vt -> "gives" | "eats" | "finds"
-  NP -> "Per" | "Kari" | "Ola" | "dinner" | "book" | Det N
-  Det -> "a" | "the"
-  N -> "book"
-  P -> "to"
-  """)
-
-
-sentence = "Kari sleeps the book".split()
-rd_parser = nltk.RecursiveDescentParser(grammar2)
-for tree in rd_parser.parse(sentence):
-    print(tree)
-
-sentence = "Ola finds".split()
-rd_parser = nltk.RecursiveDescentParser(grammar2)
-for tree in rd_parser.parse(sentence):
-    print(tree)
-
-#skrives ut ikke noe siden de er ikke lovlige sentenceer: ugrammatiske.
+"Kari sleeps the book"
+"Ola finds"
+"""
